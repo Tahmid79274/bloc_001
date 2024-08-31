0b3c4db5e622fa033b6326797cecfa01a8e49214
Tahmid. <tahmidur.rahman.79274@gmail.com>
2024-08-31 22:15:27 +0600

    First Bloc Example Complete.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ import 'dart:math' as math show Random;
 import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -23,6 +27,12 @@ class LoadPersonAction implements LoadAction {
 }
 
 enum PersonURL { persons1, persons2 }
+
+List<String> names = ['foo', 'bar'];
+
+void testIt() {
+  final baz = names[1];
+}
 
 extension UrlString on PersonURL {
   String get urlString {
@@ -64,7 +74,26 @@ class FetchResult {
 }
 
 class PersonBloc extends Bloc<LoadAction, FetchResult?> {
-  PersonBloc() : super(null);
+  final Map<PersonURL, Iterable<Person>> _cache = {};
+  PersonBloc() : super(null) {
+    on<LoadPersonAction>(
+      (event, emit) async {
+        final url = event.url;
+        if (_cache.containsKey(url)) {
+          final cachedPersons = _cache[url]!;
+          final result =
+              FetchResult(persons: cachedPersons, isRetrievedFromCache: true);
+          emit(result);
+        } else {
+          final persons = await getPersons(url.urlString);
+          _cache[url] = persons;
+          final result =
+              FetchResult(persons: persons, isRetrievedFromCache: false);
+          emit(result);
+        }
+      },
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -79,10 +108,17 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: MyHomePage(
-            // title: 'Demo Bloc',
-            ));
+        home: BlocProvider(
+          create: (_) => PersonBloc(),
+          child: MyHomePage(
+              // title: 'Demo Bloc',
+              ),
+        ));
   }
+}
+
+extension Subscript<T> on Iterable<T> {
+  T? operator [](int index) => length > index ? elementAt(index) : null;
 }
 
 class MyHomePage extends StatelessWidget {
@@ -90,7 +126,55 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    late final Bloc myBloc;
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextButton(
+                  onPressed: () {
+                    context
+                        .read<PersonBloc>()
+                        .add(LoadPersonAction(url: PersonURL.persons1));
+                  },
+                  child: Text('Load Json1')),
+              TextButton(
+                  onPressed: () {
+                    context
+                        .read<PersonBloc>()
+                        .add(LoadPersonAction(url: PersonURL.persons1));
+                  },
+                  child: Text('Load Json2'))
+            ],
+          ),
+          BlocBuilder<PersonBloc, FetchResult?>(
+            buildWhen: (previous, current) {
+              return previous?.persons != current?.persons;
+            },
+            builder: (context, state) {
+              final persons = state?.persons;
+              state?.log();
+              if (persons == null) {
+                return SizedBox();
+              }
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: persons.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(persons[index]!.name),
+                    );
+                  },
+                ),
+              );
+            },
+          )
+        ],
+      ),
+    );
   }
 }
