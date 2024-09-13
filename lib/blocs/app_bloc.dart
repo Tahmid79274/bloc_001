e6e7188428c7d4f7079607a95cfd28905ec63d7a
Tahmid. <tahmidur.rahman.79274@gmail.com>
2024-09-13 22:55:59 +0600

    test

--- lib/blocs/app_bloc.dart
+++ lib/blocs/app_bloc.dart
@@ -3,6 +3,7 @@ import 'package:bloc101/api/login_api.dart';
 import 'package:bloc101/api/notes_api.dart';
 import 'package:bloc101/blocs/action.dart';
 import 'package:bloc101/blocs/app_state.dart';
+import 'package:bloc101/model/models.dart';
 
 class AppBloc extends Bloc<AppAction, AppState> {
   final LoginApiProtocol loginApiProtocol;
@@ -11,7 +12,42 @@ class AppBloc extends Bloc<AppAction, AppState> {
   AppBloc({required this.loginApiProtocol, required this.notesApiProtocol})
       : super(const AppState.empty()) {
     on<LoginAction>(
-      (event, emit) {},
+      (event, emit) async {
+        emit(const AppState(
+            isLoading: true,
+            loginErrors: null,
+            loginHandle: null,
+            fetchedNotes: null));
+
+        final loginHandle = await loginApiProtocol.login(
+            email: event.email, password: event.password);
+
+        emit(AppState(
+            isLoading: false,
+            loginErrors: loginHandle == null ? LoginErrors.invalidHandle : null,
+            loginHandle: loginHandle,
+            fetchedNotes: null));
+      },
     );
+    on<LoadNoteAction>(
+      (event, emit) {
+        emit(AppState(
+            isLoading: true,
+            loginErrors: null,
+            loginHandle: state.loginHandle,
+            fetchedNotes: null));
+      },
+    );
+
+    final loginHandle = state.loginHandle;
+    if (loginHandle != const LoginHandle.foobar()) {
+      emit(AppState(
+          isLoading: false,
+          loginErrors: LoginErrors.invalidHandle,
+          loginHandle: loginHandle,
+          fetchedNotes: null));
+      return;
+    }
+    final notes = await notesApiProtocol.getNotes(loginHandle: loginHandle!);
   }
 }
