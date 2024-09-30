74da35a0bc36e4817c33786dded772d1b0f271e9
Tahmid. <tahmidur.rahman.79274@gmail.com>
2024-10-01 00:50:33 +0600

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void initializingFirebase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
 
 void main() {
+  initializingFirebase();
   runApp(const MyApp());
 }
 
