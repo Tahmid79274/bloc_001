4ecccd22c244411906dda9e17433ad874f7d0285
Tahmid. <tahmidur.rahman.79274@gmail.com>
2024-10-03 23:51:59 +0600

    Have to implement appp state

--- lib/auth/auth_error.dart
+++ lib/auth/auth_error.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 
-const Map<String, AuthError> authErrorMapping = {};
+const Map<String, AuthError> authErrorMapping = {
+  'user-not-found': AuthErrorUserNotFound(),
+  'weak-password': AuthErrorWeakPassword(),
+  'invalid-email': AuthErrorInvalidEmail(),
+  'operation-not-allowed': AuthErrorOperationNotAllowed(),
+  'email-already-in-use': AuthErrorEmailAlreadyInUse(),
+  'requires-recent-login': AuthErrorRequiresRecentLogin(),
+  'no-current-user': AuthErrorNoCurrentUser(),
+};
 
 @immutable
 abstract class AuthError {
@@ -10,4 +19,83 @@ abstract class AuthError {
     required this.dialogTitle,
     required this.dialogText,
   });
+
+  factory AuthError.from(FirebaseAuthException exception) =>
+      authErrorMapping[exception.code.toLowerCase().trim()] ??
+      const AuthErrorUnknown();
+}
+
+@immutable
+class AuthErrorUnknown extends AuthError {
+  const AuthErrorUnknown()
+      : super(
+          dialogText: 'Authentication Error',
+          dialogTitle: 'Unknown Authentication Error',
+        );
+}
+
+@immutable
+class AuthErrorNoCurrentUser extends AuthError {
+  const AuthErrorNoCurrentUser()
+      : super(
+          dialogText: 'No Current User',
+          dialogTitle: 'No current user with this information was found!',
+        );
+}
+
+@immutable
+class AuthErrorRequiresRecentLogin extends AuthError {
+  const AuthErrorRequiresRecentLogin()
+      : super(
+          dialogText: 'Requires recent login',
+          dialogTitle:
+              'You need to log out and log back in again in order to perrform the operation.',
+        );
+}
+
+@immutable
+class AuthErrorOperationNotAllowed extends AuthError {
+  const AuthErrorOperationNotAllowed()
+      : super(
+          dialogText: 'Operation not allowed',
+          dialogTitle: 'You can not register using this method at this moment!',
+        );
+}
+
+//auth/user-not-found
+@immutable
+class AuthErrorUserNotFound extends AuthError {
+  const AuthErrorUserNotFound()
+      : super(
+          dialogText: 'User Not found',
+          dialogTitle: 'The given user was not found on the server.',
+        );
+}
+
+@immutable
+class AuthErrorWeakPassword extends AuthError {
+  const AuthErrorWeakPassword()
+      : super(
+          dialogText: 'Weak Password',
+          dialogTitle:
+              'Please choose a stronge password consisting of more characters.',
+        );
+}
+
+@immutable
+class AuthErrorInvalidEmail extends AuthError {
+  const AuthErrorInvalidEmail()
+      : super(
+          dialogText: 'Invalid Email',
+          dialogTitle: 'Please check your email and try again.',
+        );
+}
+
+@immutable
+class AuthErrorEmailAlreadyInUse extends AuthError {
+  const AuthErrorEmailAlreadyInUse()
+      : super(
+          dialogText: 'Email already in use',
+          dialogTitle: 'Please choose another email to register with!',
+        );
 }
